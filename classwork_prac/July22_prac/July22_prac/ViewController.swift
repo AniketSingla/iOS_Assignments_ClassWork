ca238ef01655f867673248b6bd66aeb306f428ab
AniketSingla <aniket2100.be23@chitkara.edu.in>
2025-07-30 10:08:11 +0530

    Add iOS projects: PersonalityQuiz, Segue_Lab_Login, and July22_prac classwork

---
+++ classwork_prac/July22_prac/July22_prac/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  July22_prac
+//
+//  Created by Aniket Singla on 22/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("View is loaded")
+    }
+    
+
+
+}
+
